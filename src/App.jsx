9bfa134b21641944fd17ce041e0cf807b431acbc
Giorgio Grassini <giorgiograssini@gmail.com>
2022-05-25 16:34:26 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React to do list</h1>
+    </div>
+  )
+}
+
+export default App
