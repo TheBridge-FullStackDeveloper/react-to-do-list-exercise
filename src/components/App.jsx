27181f90688dba1ff1f8de653bd4d684a0d7db05
OmarjLand <omar.land2010@gmail.com>
2023-05-20 21:04:59 +0200

    Añado component MyAcitivies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,48 @@
 import { useState } from 'react'
 
+function MyActivities() {
+  const [ item, setItem ] = useState('');
+  const [ todoList, setTodoList ] = useState([]);
+
+  return(<>
+    <p>Actividad - Item: <strong><i>{item}</i></strong></p>
+
+    <ul>
+      <h2>Lista de tareas:</h2> { ' ' }
+      { todoList.map(( item ) => (
+        <li> {item} </li>
+      ))}
+    </ul>
+
+    <input 
+      type="text"
+      onChange = {(e) => {setItem( e.target.value )}} 
+    />
+
+    <br />
+
+    <button
+      onClick={() => {
+        setTodoList( [...todoList, item] )
+      }}
+    >
+    Agrega una tarea:
+    </button>
+  
+  </>
+  )
+}
+
 function App() {
+
   return (
-    <div className="App">
-      <h1>React to do list</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>React to do list</h1>
+      </div>
+
+      <MyActivities />
+    </>
   )
 }
 
