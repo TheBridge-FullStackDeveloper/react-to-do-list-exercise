6f1dd942f65c823cefbed6751601e4c281a41b4f
javier <jfernandes113@gmail.com>
2023-05-26 19:31:39 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,45 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
+  const [task, setTask] = useState('');
+  const [taskList, setTaskList] = useState([]);
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter' && task.trim() !== '') {
+      addTask();
+    }
+  };
+
+  const handleClick = () => {
+    if (task.trim() !== '') {
+      addTask();
+    }
+  };
+
+  const addTask = () => {
+    setTaskList((prevTaskList) => [...prevTaskList, task]);
+    setTask('');
+  };
+
   return (
     <div className="App">
       <h1>React to do list</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(event) => setTask(event.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={handleClick}>Add Task</button>
+      <ul>
+        {taskList.map((taskItem, index) => (
+          <li key={index}>{taskItem}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
