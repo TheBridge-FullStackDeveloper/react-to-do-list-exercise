9e1400dee903ea7496e464facde818a40f144004
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-05-26 21:12:20 +0200

    viernes

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,13 +1,27 @@
 import { useState } from "react";
 
 function Todolist () {
-    const [nombre, setName]=useState("");
+    const [name, setName]=useState("");
+    const [studentList, setStudentList]= useState([]);
     return (
-        <>
-        <p>Name:{nombre}</p>
-        <input type="text" onChange={(e)=>{
+    <>
+        <p>Name:{name}</p>
+        <ul>
+            Alumnos: {studentList.map(student => <li>{student}</li> )}
+        </ul>
+        <input 
+        type="text"
+        onChange={(e)=>{
             setName(e.target.value);
-        }}/>
+        }}
+
+    />
+        <button 
+        onClick={() => {
+            setStudentList([...studentList,name]);
+        }}
+        >
+            Add student to the list </button>
         </>
     )
     ;
