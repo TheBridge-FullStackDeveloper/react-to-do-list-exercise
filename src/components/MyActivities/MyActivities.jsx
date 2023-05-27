160b215bb8df8d9bbfc387fde84777ba12241f23
OmarjLand <omar.land2010@gmail.com>
2023-05-27 22:53:13 +0200

    Añado funcion en Intro al meter datos

--- src/components/MyActivities/MyActivities.jsx
+++ src/components/MyActivities/MyActivities.jsx
@@ -2,38 +2,57 @@ import { useState } from 'react';
 import "./MyActivities.css"
 
 function MyActivities() {
-    const [ item, setItem ] = useState(''); 
-    const [ todoList, setTodoList ] = useState([]); 
-    
+    const [ item, setItem ] = useState('');
+    const [ todoList, setTodoList ] = useState([]);
+
     // Función para el cambio el input para mostrar mientras edito
     const handleItemChange = ( e ) => {
-      setItem( e.target.value )
+      setItem( e.target.value );
     }
+
+    // Función para agregar un item en el Array
+    const handleAddItem = () => {
+      if( item.trim() != "" ){
+        setTodoList( [ ...todoList, item ]);
+        // setItem('');
+      }
+    };
+
+    // Función que al presionar intro añade una tarea directamente
+    const addItemByPressEnter = (e) => {
+      if ( e.key === 'Enter' ) {
+        e.preventDefault();
+        handleAddItem();
+      }
+    }
+
+
 
     //Función que tacha la actividad una vez es seleccionada como realizada
     const manageMarkDone = ( index ) => {
       const newTodos = [ ...todoList ]
       newTodos[ index ] = <del> { newTodos[ index ] } </del>
       setTodoList( newTodos )
-    } 
+    }
 
 
     // Función que me permite eliminar un item
-    const handleDeleteTodo = ( index ) => {
+    const handleDeleteItem = ( index ) => {
       const newTodos = [ ...todoList ];
       newTodos.splice( index, 1 );
       setTodoList( newTodos );
     };
-  
+
     return(<>
       <section className="main-container">
 
         <p className="margin-text">Actividad - Item: <strong><i>{ item }</i></strong></p>
-        
+
         {/* Para meter los datos */}
-        <input 
+        <input
           type="text"
-          onChange={ handleItemChange } 
+          onChange={ handleItemChange }
+          onKeyPress={ addItemByPressEnter }
           className="margin-text input-text"
           placeholder="¿Qué harás?"
         />
@@ -41,21 +60,22 @@ function MyActivities() {
         <ul className="align-left">
 
           <h2>Lista de tareas:</h2> { ' ' }
-          {todoList.map(( item, index ) => ( 
+          {todoList.map(( item, index ) => (
 
             <div className='form-contain'>
 
-              <section className="item-flex " key={ index }>
+              <section className="item-flex" key={ index }>
                 <label htmlFor={ item }>{ item }</label>
                 <section className="check-add-rmv">
 
-                  <input type="checkbox" 
-                         value="task"
-                         onClick={ () => manageMarkDone( index ) } 
+                  <input type="radio"
+                         value={item}
+                         name={item}
+                         onClick={ () => manageMarkDone( index ) }
                   />
 
                   <button className="btn-delete"
-                    onClick={ () => handleDeleteTodo( index ) }
+                    onClick={ () => handleDeleteItem( index ) }
                   >X</button>
 
                 </section>
@@ -69,7 +89,8 @@ function MyActivities() {
 
         <br />
 
-        <button onClick={() => { setTodoList( [ ...todoList, item ])}} className="btn-addTask">
+        {/* <button onClick={() => { setTodoList( [ ...todoList, item ])}} className="btn-addTask"> */}
+        <button onClick={ handleAddItem } className="btn-addTask">
           Añadir tarea
         </button>
 
