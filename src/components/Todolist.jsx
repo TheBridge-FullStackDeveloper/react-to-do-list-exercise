73ff9c9d6f90d81f33609650490daca619d146c6
Auxi Huertas <2bniauxihuertas@gmail.com>
2023-05-28 13:02:58 +0200

    remove task

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,33 +1,61 @@
 import { useState } from "react";
 
-function Todolist () {
-    const [task, setTask]=useState("");
-    const [taskList, setTaskList]= useState([]);
-    return (
+function Todolist() {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+  const handleKeyPress = (e) => {
+    // console.log(e);
+    if (e.key === "Enter") {
+      addTaskToList();
+    }
+  };
+  const addTaskToList = () => {
+    if (task.trim() !== "") {
+      setTaskList([...taskList, task]);
+      setTask("");
+    }
+    
+  };
+  const removeTask = (index) => {
+    const updatedTaskList = [...taskList];
+    updatedTaskList.splice(index, 1);
+    setTaskList(updatedTaskList);
+  };
+  
+  return (
     <>
-        {/* <p>Task name :{task}</p> */}
-        <input 
+      {/* <p>Task name :{task}</p> */}
+      <input
         type="text"
-        value = "Task"
-        onChange={(e)=>{}
-            setTask(e.target.value);
+        onChange={(e) => {
+          setTask(e.target.value);
         }}
-
-    />
-        <button 
+        onKeyPress={handleKeyPress}
+      />
+      <button
         onClick={() => {
-            setTaskList([...taskList,task]);
+          setTaskList([...taskList, task]);
         }}
-        >
-            Add task to the list </button>
-            
-        <ul>
-            Tasks to do: {taskList.map(task => <li>{task}</li> )}
-        </ul>
-        </>
-        
-    )
-    ;
-};
+      >
+        Add task to the list{" "}
+      </button>
+
+      <ul>
+        Tasks to do:{" "}
+        {taskList.map((task,index) => (
+          <li>{task} <button onClick={() => removeTask(index)}>Remove</button></li>
+        ))}
+      </ul>
+      {/* <ul>
+        {taskList.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul> */}
+    </>
+  );
+}
 
 export default Todolist;
